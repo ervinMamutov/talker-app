bbcc4dfd932683878914cc918b5f7e7ab43bea6d
ervinMamutov <ervin.news@gmail.com>
2023-11-20 14:48:35 +0100

    added logger

---
+++ backend/middleware/logger.js
@@ -0,0 +1,13 @@
+import chalk from 'chalk';
+
+const logger = (req, res, next) => {
+  const timestamp = new Date().toUTCString();
+  console.log(`
+  url: ${chalk.green(req.url)} 
+  --- method: ${chalk.blue(req.method)} 
+  --- status: ${chalk.yellow(res.statusCode)} 
+  --- date: ${chalk.purpure(timestamp)}`);
+  next();
+};
+
+export default logger;
