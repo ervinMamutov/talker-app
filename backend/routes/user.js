a4caa858d28d49028b97825b1471d1b529fb9e31
ervinMamutov <ervin.news@gmail.com>
2023-11-20 22:37:11 +0100

    added router user

---
+++ backend/routes/user.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import userControllers from '../controllers/user.js';
+
+const router = express.Router();
+
+router.post('/register', userControllers.register);
+router.post('/login', userControllers.login);
+router.post('/logout', userControllers.logout);
+
+export default router;
